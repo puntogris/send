f4270c4420acddcb3149343abb09f8d325a99cd2
puntogris <rosariodota3@gmail.com>
2024-01-13 16:27:39 -0300

    added header and toolbar link

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,14 +7,18 @@
 
 <div class="flex h-screen min-h-screen flex-col bg-gradient-to-br from-gray-100 to-blue-50">
 	<header class="flex items-center justify-between p-4">
-		<h1 class="text-xl font-medium">send.</h1>
+		<a href="/" class="text-xl font-medium">send.</a>
 		<button class="rounded-md border border-blue-600 p-2 text-blue-600">Sign in/up</button>
 	</header>
 	<div class="mx-16 my-8 h-full rounded-xl bg-white shadow-lg">
 		<slot />
 	</div>
 	<footer class="mt-auto flex items-center justify-between px-6 py-4">
-		<h3 class="text rounded-sm bg-black p-1 font-bold text-white">https://puntogris.com</h3>
+		<a
+			href="https://www.puntogris.com/"
+			target="_blank"
+			class="text rounded-sm bg-black p-1 font-bold text-white">https://puntogris.com</a
+		>
 		<div class="flex gap-3">
 			<h4 class="text-sm text-gray-500">Contact</h4>
 			<h4 class="text-sm text-gray-500">Github</h4>
