e730dfa3a7790a26a69ba57f7281309f786483a2
puntogris <rosariodota3@gmail.com>
2024-01-20 13:24:50 -0300

    added date desc order in uploads page and filter by not expired ones

--- src/routes/uploads/+page.server.ts
+++ src/routes/uploads/+page.server.ts
@@ -1,14 +1,15 @@
 import { db } from '$lib/server/drizzle.js';
 import { uploads } from '$lib/server/schema.js';
 import { error } from '@sveltejs/kit';
+import { desc, gte } from 'drizzle-orm';
 
 /** @type {import('./$types').LayoutServerLoad} */
-export async function load({ params }) {
-	const upload = await db.select().from(uploads);
+export async function load() {
+	const upload = await db.select().from(uploads).orderBy((desc(uploads.createdAt))).where(gte(uploads.expireAt, new Date()));
 
 	if (!upload) {
 		return error(404, 'Files not found');
 	}
 
-	return { upload };
+	return { uploads: upload };
 }
--- src/routes/uploads/+page.svelte
+++ src/routes/uploads/+page.svelte
@@ -14,7 +14,9 @@
 	function getFormattedDate(date: Date) {
 		return date.toLocaleDateString('en-US', {
 			day: 'numeric',
-			month: 'short'
+			month: 'short',
+			hour: '2-digit',
+			minute: '2-digit'
 		});
 	}
 </script>
@@ -22,7 +24,7 @@
 <article class="flex w-full flex-col gap-4 p-6">
 	<h1 class="mb-6 text-3xl font-bold">My uploads</h1>
 	<div class="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-		{#each data.upload as upload}
+		{#each data.uploads as upload}
 			<div
 				class="overflow-hidden rounded-lg bg-gradient-to-br from-purple-100 to-indigo-100 shadow-lg"
 			>
