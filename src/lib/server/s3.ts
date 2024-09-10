1aa1aef211a36021edfab378f9384741ecd18d85
puntogris <rosariodota3@gmail.com>
2024-09-09 23:51:30 -0300

    renamed env variables

--- src/lib/server/s3.ts
+++ src/lib/server/s3.ts
@@ -2,14 +2,14 @@ import {
 	PRIVATE_S3_KEY_ID,
 	PRIVATE_S3_SECRET_KEY,
 	PRIVATE_S3_URL_EXPIRE_TIME,
-	PRIVATE_STORAGE_ENDPOINT,
-	PRIVATE_STORAGE_REGION
+	PRIVATE_S3_ENDPOINT,
+	PRIVATE_S3_REGION
 } from '$env/static/private';
 import { S3Client } from '@aws-sdk/client-s3';
 
 export const s3 = new S3Client({
-	region: PRIVATE_STORAGE_REGION,
-	endpoint: PRIVATE_STORAGE_ENDPOINT,
+	region: PRIVATE_S3_REGION,
+	endpoint: PRIVATE_S3_ENDPOINT,
 	credentials: {
 		accessKeyId: PRIVATE_S3_KEY_ID,
 		secretAccessKey: PRIVATE_S3_SECRET_KEY
--- src/routes/api/upload/s3/+server.ts
+++ src/routes/api/upload/s3/+server.ts
@@ -2,7 +2,7 @@ import { error, json } from '@sveltejs/kit';
 import { PutObjectCommand } from '@aws-sdk/client-s3';
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 import { s3, expiresIn } from '$lib/server/s3.js';
-import { PRIVATE_STORAGE_BUCKET, PRIVATE_S3_PREFIX } from '$env/static/private';
+import { PRIVATE_S3_BUCKET, PRIVATE_S3_PREFIX } from '$env/static/private';
 
 export const POST = async ({ locals }) => {
 	if (!locals.authenticated) {
@@ -14,7 +14,7 @@ export const POST = async ({ locals }) => {
 	const url = await getSignedUrl(
 		s3,
 		new PutObjectCommand({
-			Bucket: PRIVATE_STORAGE_BUCKET,
+			Bucket: PRIVATE_S3_BUCKET,
 			Key: key,
 			ContentType: 'application/octet-stream'
 		}),
