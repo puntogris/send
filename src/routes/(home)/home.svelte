0e859219793e93eccbc5ac54721ecb73c895da1e
puntogris <rosariodota3@gmail.com>
2025-11-30 13:40:37 -0500

    added drag and drop files to start

--- src/routes/(home)/home.svelte
+++ src/routes/(home)/home.svelte
@@ -1,16 +1,50 @@
 <script lang="ts">
 	import CirclePlusIcon from '$lib/icons/circlePlusIcon.svelte';
 	import { getFilesStore } from '$lib/stores';
+	import { twMerge } from 'tailwind-merge';
 
 	const filesStore = getFilesStore();
+	let isDragging: boolean = false;
+
+	function handleFiles(fileList: FileList) {
+		if (fileList) {
+			filesStore.addFiles(fileList);
+		}
+	}
+
+	function handleDragOver(event: DragEvent) {
+		event.preventDefault();
+		isDragging = true;
+	}
+
+	function handleDragLeave(event: DragEvent) {
+		isDragging = false;
+	}
+
+	function handleDrop(event: DragEvent) {
+		event.preventDefault();
+		event.stopPropagation();
+		isDragging = false;
+
+		if (event.dataTransfer && event.dataTransfer.files) {
+			handleFiles(event.dataTransfer.files);
+		}
+	}
 </script>
 
 <div
-	class="flex h-full flex-col justify-center rounded-md border-4 border-dashed border-gray-300 p-4"
+	class={twMerge(
+		'flex h-full flex-col justify-center rounded-md border-4 border-dashed p-4',
+		isDragging ? 'border-gray-400' : 'border-gray-300'
+	)}
+	on:dragover={handleDragOver}
+	on:dragleave={handleDragLeave}
+	on:drop={handleDrop}
+	role="region"
 >
 	<div class="flex flex-col items-center gap-8">
 		<CirclePlusIcon class="size-14 text-blue-600" />
-		<div class="flex flex-col items-center gap-1">
+		<div class=" flex flex-col items-center gap-1">
 			<h3 class="text-lg font-semibold">Drag and drop files</h3>
 			<h2>or click to send up to 1GB</h2>
 		</div>
