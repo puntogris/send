5ccfb78c1de01b698506f90c1dce4e6bfb1204ee
puntogris <rosariodota3@gmail.com>
2025-12-02 13:50:07 -0500

    improved ui in mobile

--- src/routes/(home)/+page.svelte
+++ src/routes/(home)/+page.svelte
@@ -7,17 +7,17 @@
 	let hasFilesSelected = $derived(filesStore.files.length > 0);
 </script>
 
-<div class="grid grow gap-8 p-3 max-md:grid-rows-2 sm:p-6 md:grid-cols-2">
+<div class="grid grow gap-2 p-3 max-lg:grid-rows-1 sm:p-6 lg:grid-cols-2 lg:gap-8">
 	{#if hasFilesSelected}
 		<Upload />
 	{:else}
 		<Home />
 	{/if}
-	<div class="flex flex-col gap-4 pt-6 md:pt-16">
-		<h1 class="text-3xl font-bold">Simple, private file sharing</h1>
-		<p>
-			Send lets you share files with end-to-end encryption and a link that automatically expires.<br
-			/>
+	<div class="flex flex-col gap-2 p-2 lg:gap-4 lg:p-6 lg:pt-16">
+		<h1 class="text-lg font-bold lg:text-3xl">Simple, private file sharing</h1>
+		<p class="text-sm text-gray-600 lg:text-base">
+			Send lets you share files with end-to-end encryption and a link that automatically expires.
+			<br />
 			So you can keep what you share private and make sure stuff doesn't stay online forever.
 		</p>
 	</div>
